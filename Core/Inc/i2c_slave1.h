f52ae233abe3a7c4b6c26a65f6fa8374392f06c0
Muhammad Salman <sk0735260@gmail.com>
2025-06-14 17:03:37 +0500

    test : testing the github integration for first
    Fill the requried directory to be used
    
    commit : my first commit

---
+++ Core/Inc/i2c_slave1.h
@@ -0,0 +1,13 @@
+/*
+ * i2c_slave1.h
+ *
+ *  Created on: Sep 29, 2024
+ *      Author: MSI
+ */
+
+#ifndef INC_I2C_SLAVE1_H_
+#define INC_I2C_SLAVE1_H_
+
+
+
+#endif /* INC_I2C_SLAVE1_H_ */
---
+++ Core/Src/i2c_slave1.c
@@ -0,0 +1,15 @@
+/*
+ * i2c_slave1.c
+ *
+ *  Created on: Sep 29, 2024
+ *      Author: MSI
+ */
+
+
+
+
+#include "main.h"
+
+#include "i2c_slave1.h"
+
+
